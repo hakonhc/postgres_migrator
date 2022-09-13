21f9c59dd570dd839c9eaf620af648757d988931
Håkon Clausen <haakon@sunstone.care>
2022-09-13 12:31:50 +0200

    Support that schema files can be stored in sub folders

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{io::{self, Read, Write}, fs, path::PathBuf};
 use chrono::Utc;
 use postgres::Config;
 use anyhow::{anyhow, Result, Context};
+use walkdir::WalkDir;
+
 
 fn create_timestamp() -> String {
 	Utc::now().format("%Y%m%d%H%M%S").to_string()
@@ -63,11 +65,13 @@ fn list_sql_files(directory: &str) -> io::Result<Vec<PathBuf>> {
 	let mut entries = vec![];
 	let sql_extension = Some(std::ffi::OsStr::new("sql"));
 
-	for entry in fs::read_dir(directory)? {
+	for entry in WalkDir::new(directory) {
 		let entry = entry?;
 		let path = entry.path();
 		if !path.is_dir() && path.extension() == sql_extension {
-			entries.push(path);
+			let mut path_buf = PathBuf::new();
+			path_buf.push(path);
+			entries.push(path_buf);
 		}
 	}
 	entries.sort();
@@ -87,12 +91,17 @@ fn test_list_sql_files() -> io::Result<()> {
 	fs::File::create("migrations/40.txt")?;
 	fs::File::create("migrations/yo")?;
 	fs::create_dir("migrations/agh")?;
+	fs::File::create("migrations/agh/40_yoyo.sql")?;
+	fs::create_dir("migrations/agh/doh")?;
+	fs::File::create("migrations/agh/doh/50_yoyo.sql")?;
 
 	let migration_files = list_sql_files(DEFAULT_MIGRATIONS_DIRECTORY)?;
 	assert_eq!(migration_files, vec![
 		PathBuf::from("migrations/10_yo.sql"),
 		PathBuf::from("migrations/20_yo.sql"),
 		PathBuf::from("migrations/30_yo.sql"),
+		PathBuf::from("migrations/agh/40_yoyo.sql"),
+		PathBuf::from("migrations/agh/doh/50_yoyo.sql"),
 	]);
 
 	purge_migrations_directory(DEFAULT_MIGRATIONS_DIRECTORY)?;
